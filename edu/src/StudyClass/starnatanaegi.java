1e63a1838c5b07e9b62c9f4fee82374dc844fa5f
YeDongVibe <wkwhstladmss@naver.com>
2023-04-13 19:52:52 +0900

    스터디1일차

---
+++ edu/src/StudyClass/starnatanaegi.java
@@ -0,0 +1,16 @@
+package StudyClass;
+
+public class starnatanaegi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<10; i++) {
+			for(int j = 0; j<i; j++) {
+				System.out.print(" ");
+			}
+			System.out.print("*");
+			System.out.print("\n"); //줄바꿈
+		}
+	}
+
+}
