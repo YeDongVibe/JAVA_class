a1b6f51d7368351af47cc039e0c53077aa0b94ca
YeDongVibe <wkwhstladmss@naver.com>
2023-04-13 19:41:45 +0900

    스터디 1일차

---
+++ edu/src/StudyClass/gugudan.java
@@ -0,0 +1,15 @@
+package StudyClass;
+
+public class gugudan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int j = 2; j < 10; j++) {
+			for (int i = 1; i < 10; i++) {
+				System.out.println(j + " * " + i + " = " + (j*i));
+			}
+
+		}
+
+	}
+}
