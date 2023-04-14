9769b6b37e6f1d3361db9ea90705c1bc9e7d1657
YeDongVibe <wkwhstladmss@naver.com>
2023-04-14 09:17:38 +0900

    문제

--- edu/src/Chapter8/Problem.java
+++ edu/src/Chapter8/Problem.java
@@ -1,28 +1,29 @@
 package Chapter8;
 
 import java.util.Scanner;
-
 public class Problem {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
 		int num = 0;
 		int res1 = 0;
-		int [] arr = new int [i];
+		int[] arr = new int [8];
 		
+		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자를 입력하세요");
 		num = sc.nextInt();
 		
-		for(int i = num; i>0; i++) {
-			res1 = num %2;
-			num = num/2;
-			if (num == 0) break;
+		while(true) {
+			arr[res1]= num % 2;
+			num = num /2;
+			res1++;
+			if(num == 0) {
+				break;
+			}
 		}
-		System.out.println(arr[i]);
-		sc.close();
-
+		for(int i = (res1-1);i>=0;i--) {
+			System.out.print(arr[i]);
+		}
+		
+		sc.close();	
 	}
-
 }
--- edu/src/Chapter8/Problem2.java
+++ edu/src/Chapter8/Problem2.java
@@ -5,25 +5,29 @@ import java.util.Scanner;
 public class Problem2 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int num[] = new int[2];
-		int max = 0;
-		int min = 0;
-		int rem = 0;
-
 		System.out.println("숫자를 입력하세요");
-		for (int i = 0; i < num.length; i++) {
-			num[i] = sc.nextInt();
-			if (max <= num[i]) {
-				max = num[i];
-			} else {
-				min = num[i];
-			}
-			//whilerem = max % min;
-			if(rem == 0) { 
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int rem = 0;
+		int rem2 = 0;
+		int rem3 = 0;
+		
+		if (num1 < num2) {
+			rem = num2 % num1;
+		
+			System.out.println("최대공약수는 " + rem);
+			
+		} else if (rem == 0) {
+			int gongyakso = num1;
+			int gongbaeso = (num1 * num2) / gongyakso;
+			System.out.println("최소공배수는 " + gongbaeso);
+			System.out.println("최대공약수는 " + gongyakso);
+		} else {
+			num2 = num1;
+			num1 = rem;
 		}
 
+		sc.close();
 	}
-
 }
