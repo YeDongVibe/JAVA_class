28534118db3a24db974aa601447bb692434b8b88
YeDongVibe <wkwhstladmss@naver.com>
2023-04-17 17:55:43 +0900

    23.04.17

--- edu/src/Chapter08/Problem2.java
+++ edu/src/Chapter08/Problem2.java
@@ -10,41 +10,38 @@ public class Problem2 {
 		int num1 = sc.nextInt();
 		int num2 = sc.nextInt();
 		int rem = 0;
-//		int rem2 = 0;
-//		int rem3 = 0;
-		
+		int gongbaeso = 0;
+		int gongyakso = 0;
+
+
 		if (num1 < num2) {
 			rem = num2 % num1;
-		
-			System.out.println("최대공약수는 " + rem);
-			
-		 while(rem == 0) {
-			 if (rem ==0) {
-			 int gongyakso = num1;
-				int gongbaeso = (num1 * num2) / gongyakso;
-				System.out.println("최소공배수는 " + gongbaeso);
-				System.out.println("최대공약수는 " + gongyakso);
-		 }
-		
-		
-		
-		else if (rem == 0) {
-			int gongyakso = num1;
-			int gongbaeso = (num1 * num2) / gongyakso;
-			System.out.println("최소공배수는 " + gongbaeso);
+
+			while (rem > 0) {
+				gongyakso = rem;
+				rem = num1 % rem;
+
+			}
 			System.out.println("최대공약수는 " + gongyakso);
+
+			gongbaeso = (num1 / gongyakso) * (num2 / gongyakso) * gongyakso;
+
+			System.out.println("최소공배수는 " + gongbaeso);
 		} else {
-			num2 = num1;
-			num1 = rem;
-			num2 = num1;
-			num1 = rem;
+			rem = num1 % num2;
+
+			while (rem > 0) {
+				gongyakso = rem;
+				rem = num2 % rem;
+
+			}
+			System.out.println("최대공약수는 " + gongyakso);
+
+			gongbaeso = (num1 / gongyakso) * (num2 / gongyakso) * gongyakso;
+
+			System.out.println("최소공배수는 " + gongbaeso);
 		}
 		sc.close();
-		
-		}
-
-		
 	}
 
-	}
 }
--- edu/src/Chapter08/Problem4.java
+++ edu/src/Chapter08/Problem4.java
@@ -11,26 +11,31 @@ public class Problem4 {
 		int s = 1;
 		int oh = 1;
 		int res = 0;
+		int tores = 0;
 		
 		System.out.println("숫자를 입력하세요");
 		num = sc.nextInt();
 		res = num - (2+3+5);
 		
-		if (res> 5) {
-			res = res-5*(res % 5);
+		if (res % 5==0) {
+			res = res-(5*(res / 5));
+			tores += res % 5;
 		}
-		else if (res>3) {
-			res = res + res / 5;
-			res = res- 3*(res % 3);
+		else if (res % 3==0) {
+			res = res- 3*(res / 3);
+			tores += res % 3;
 		}
-		else {
-			res = res + res / 3;
-			res = res - 2*(res % 2);
+		else if(res % 2 ==0 ) {
+			res = res - 2*(res / 2);
+			tores += res % 2;
+		}else if (res /5 !=0 || res /3 !=0 || res/2 !=0) {
+			
 		}
+		
 		
 	
 		System.out.println(res);
-		System.out.println("총 추의 수(5g, 3g, 2g) : " + oh + " ," + s + " ," + e + "개");
+		System.out.println("총 추의 수(5g, 3g, 2g) : " + oh + " ," + s + " ," + e + "개" + " : 총 " + tores + "개");
 		sc.close();
 
 	}
