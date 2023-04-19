e5fc740db9cc920ff6ab8e831087710f5896c329
YeDongVibe <wkwhstladmss@naver.com>
2023-04-19 11:27:48 +0900

    23.04.19

--- edu/src/Chapter10/Test06.java
+++ edu/src/Chapter10/Test06.java
@@ -1,6 +1,7 @@
 package Chapter10;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class Test06 {
 
@@ -9,17 +10,26 @@ public class Test06 {
 		String word[] = { "BUMBLEBEE", "HEAVEN", "ALTHOUGH", "WONDER" };
 		String meaning[] = { "꿀벌과에 속하는 호박벌", "천국", "그럼에도 불구하고", "호기심이 들다" };
 
-		HashMap<String, String> dic = new HashMap<String, String>();
+		HashMap<String, String> dic = new HashMap<String, String>(); //String 타입의 키값과 String 타입의 값을 저장하는 HashMap 생성
 
 		for (int i = 0; i < word.length; i++) {
-			dic.put(word[i], meaning[i]);
+			dic.put(word[i], meaning[i]); //key값
 		}
 
 		System.out.println(dic);
 		System.out.println(dic.size()); // 4
 
-		System.out.println(dic.keySet()); // [HEAVEN, BUMBLEBEE, WONDER, ALTHOUGH]
-		System.out.println(dic.values()); // [천국, 꿀벌과에 속하는 호박벌, 호기심이 들다, 그럼에도 불구하고]
+		System.out.println("toString : " + dic.toString());
+		System.out.println("KeySet : " + dic.keySet()); // [HEAVEN, BUMBLEBEE, WONDER, ALTHOUGH]
+		System.out.println("values : " + dic.values()); // [천국, 꿀벌과에 속하는 호박벌, 호기심이 들다, 그럼에도 불구하고]
+		
+		Iterator<String> keys = dic.keySet().iterator();
+		while (keys.hasNext()) {
+			String key = keys.next();
+			System.out.println("iterator getkey " + dic.get(key));
+		}
+		
+//--------------------------------------------------------------------------------------------------------------
 
 		System.out.println("HEAVEN : " + dic.get("HEAVEN"));
 		dic.replace("HEAVEN", "아주 행복한 감정");
