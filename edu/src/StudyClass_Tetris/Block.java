1a70c5324036da0e51a92b326be0dd5c5b930c2d
YeDongVibe <wkwhstladmss@naver.com>
2023-04-19 20:14:08 +0900

    23.04.19 study

--- edu/src/StudyClass_Tetris/Block.java
+++ edu/src/StudyClass_Tetris/Block.java
@@ -2,12 +2,78 @@ package StudyClass_Tetris;
 
 //class안에 변수나 함수를 만들때는 무조건 Private으로 시작해라. 이후 사용할 때에 public 혹은 protected로 바꿔서 사용하는게 보안적으로도 좋다.
 
-public class Block {
-	
-	private int a; 
-	private String b;
+//떨어짐 : yPos, 이동 : xPos, 회전 : rotation
 
-	protected void Drop () { //TetrisMain에서 b.Drop()에서 괄호속에 아무것도 없으므로 여기서도 아무것도 안써야함.
-		System.out.println("블럭이 떨어지고 있습니당당");
+public class Block {
+
+	// field, 지역변수 선언
+	private String color;
+	private String shape;
+	private int rotation;
+	private int xPos;
+	private int yPos;
+
+	// 생성자 만들기 : TetrisMain의 new Block에서 Block과 관련한 내용 만들기.
+	public Block(String c, String s) { // color과 shape받는 파라미터 만들기
+
+		this.color = c;
+		this.shape = s;
+		this.rotation = 0;
+		this.xPos = 4;
+		this.yPos = 20;
+
+	}
+
+	protected void Drop() { // TetrisMain에서 b.Drop()에서 괄호속에 아무것도 없으므로 여기서도 아무것도 안써야함.
+
+		while (this.yPos > 0) { //계속 아래의 출력을 반복.yPos을 위한 조건 제시.
+
+			System.out.println("블럭" + this.toString());// this : 클래스 안에서 사용할 떄 this.~~를 쓰면 함수등을 호출할 때 사용.
+			this.yPos--;
+			
+			//1초를 쉬도록 하는 코드
+			try {
+				Thread.sleep(1000); //try~catch문을 필수로 써야함. / Thread.sleep(숫자) : 
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public String toString() { //이걸 왜한건지 이해가 되지않으니 내일 여쭈어봐야겠담.....7장이후 내용한대용
+		String msg = "color = " + this.color + ", shape = " + this.shape + ", rotation = " + this.rotation + ", xPos = "
+				+ this.xPos + ", yPos = " + this.yPos;
+		return msg;
+
+		// 위의 내용을 이렇게 써도 무관.
+		// return "color = " + this.color + ", shape = " + this.shape + ", rotation = "
+		// + this.rotation + ", xPos = " + this.xPos + ", yPos = " + this.yPos;
+
+	}
+
+	protected void MoveLeft() { //xPos이 감소하면서 이동
+		this.xPos--;
+
+	}
+
+	protected void MoveRight() {
+		this.xPos++;
+
+	}
+
+	protected void MoveAcc() {
+		this.yPos--;
+
+	}
+
+	protected void Rotate() {
+		this.rotation += 90; //90도씩 증가.
+		this.rotation = this.rotation % 360;// = %=360이라고 써도 ㄱㅊ (아래의 if문과 같은 결과)
+		
+//		if(this.rotation >= 360) { //0, 90, 180, 360, 0으로 돌아가기 위한 반복문.
+//			this.rotation = 0;
+//		}
+
 	}
 }
--- edu/src/StudyClass_Tetris/TetrisMain.java
+++ edu/src/StudyClass_Tetris/TetrisMain.java
@@ -1,14 +1,79 @@
 package StudyClass_Tetris;
 
-public class TetrisMain {
+import java.util.Random;
+import java.util.Scanner;
+
+public class TetrisMain implements Runnable {
+
+	static private Block b; // main과 run main에서 사용하기 위해 밖에서 선언.
 
 	public static void main(String[] args) {
-		
-		//블럭만들기
-		Block b = new Block();
-		
-		//블럭 떨어트리기
-		b.Drop();
+
+		Thread cmd = new Thread(new TetrisMain());// 시작되는 시점부터 run이라는 Main함수가 실행된다.
+		cmd.start(); // 시작되는 시점부터 run이라는 Main함수가 실행된다.
+
+		while (true) {
+
+			// 블럭을 만들때 white와 ㄱ을 랜덤하게 바꾸기
+			String[] arrColor = new String[] { "white", "Purple", "Red", "Green", "Orange" }; // color 지정
+			String[] arrShape = new String[] { "ㄱ", "ㄴ", "ㄹ", "ㅁ", "ㅣ" }; // shape 지정
+
+			// 랜덤수(0~4) 생성 후 arrColor, arrShape에 적용
+			Random rand = new Random(System.currentTimeMillis());
+
+			int r1 = rand.nextInt(5); // 0부터 4까지 숫자 랜덤하게 생성.
+			int r2 = rand.nextInt(5);
+//			System.out.println("r1 : " + r1); //r1, r2값 확인용.
+//			System.out.println("r2 : " + r2);
+
+			// 블럭만들기
+			b = new Block(arrColor[r1], arrShape[r2]); // 문자열을 넣어 받을 수 있는 파라미터. //r1, r2의 랜덤수를 받아 각 선택값을 랜덤하게 출력.
+
+			// 블럭 떨어트리기 : 동시에 실행하기에는 Thread를 한 곳에서 사용할 수 없기에 두개의 곳에서 사용해 적용.(Runnable이라는
+			// 클래스를 인터페이스하여 동작)
+			b.Drop();
+
+		}
+	}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	@Override
+	public void run() { // Implements Runnable을 선언해주기 위한 main 함수생성.
+		Scanner sc = new Scanner(System.in);
+		boolean exit = false;
+		while (true) {// 방향지정.
+			String c = sc.nextLine();
+
+			switch (c) {
+			case "a":
+				b.MoveLeft();
+				break;
+			case "d":
+				b.MoveRight();
+				break;
+			case "s":
+				b.MoveAcc();
+				break;
+			case "w":
+				b.Rotate();
+				break;
+			case "x":
+				exit = true; // 프로그램을 종료하기 위한 코드
+				break;
+			}
+			if (exit == true) {
+				break;
+			}
+
+			// 1초를 쉬도록 하는 코드
+			try {
+				Thread.sleep(1000); // try~catch문을 필수로 써야함. / Thread.sleep(숫자) : 숫자만큼 쉬기.
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		sc.close();
 
 	}
 
