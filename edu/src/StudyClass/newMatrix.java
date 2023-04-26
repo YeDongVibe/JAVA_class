3469fe472d3ab0cc25bcb2b719a0ec7bfb6efaad
YeDongVibe <wkwhstladmss@naver.com>
2023-04-26 20:13:34 +0900

    23.04.26 study

--- edu/src/StudyClass/newMatrix.java
+++ edu/src/StudyClass/newMatrix.java
@@ -5,27 +5,33 @@ public class newMatrix {
 	public static void main(String[] args) {
 		newMatrixclass A = new newMatrixclass(2, 3);
 		newMatrixclass A1 = new newMatrixclass(2, 3);
-		// newMatrixclass B = new newMatrixclass();
-		// newMatrixclass C = new newMatrixclass();
-		// newMatrixclass D = new newMatrixclass();
+		newMatrixclass B = new newMatrixclass(3, 4);
+
 		
 		
 		A.setData();
 		A1.setData();
+		B.setData();
 		
 		A.showData("A");
 		A1.showData("A1");
+		B.showData("B");
 		
 
 		newMatrixclass A2 = newMatrixclass.addMatrix(A, A1); // matrix A와 A1을 집어넣어 A2생성. //클래스이름.addMatrix(함수)(변수1, 변수2); //클래스를 만들어 가져와 사용함.
 		A2.showData("A2");
-		int row = A2.getRowCount(); //파라미터 없고 return을 받아 row로 감
-		System.out.println(row);
+		//int row = A2.getRowCount(); //파라미터 없고 return을 받아 row를 print해줌.
+		//System.out.println(row); //A를 (2,3)으로 만들었기에 2가 출력됨.
 		
-		A2.setRowCount(5); // 파라미터 있고 return값을 받을 것이 없음. = 함수는 void면 된다.
-		System.out.println(A2.getRowCount());
+		//A2.setRowCount(A2.getRowCount()); // 파라미터 있고 return값을 받을 것이 없음. = 함수는 void면 된다.
+		//System.out.println(A2.getRowCount()); //<5로 값을 변경했기에 5로 출력됨.> 이후 값의 변경위험때문에 getRowCount로 변경함.그러면 A2을 받아 출력됨.
 		
-
+		newMatrixclass D = A.Transpose();
+		D.showData("D");
+		
+		newMatrixclass C = newMatrixclass.multiplyMatix(A, B);
+		C.showData("c");
+		
 	}
 
 }
--- edu/src/StudyClass/newMatrixclass.java
+++ edu/src/StudyClass/newMatrixclass.java
@@ -42,7 +42,8 @@ public class newMatrixclass {
 				data[r][c] = rand.nextInt(100);
 			}
 		}
-		try {
+		try { // 위의 currentTimeMillis만 사용시 너무나도 좋은 성능의 컴퓨터 덕분에 시간차를 인지 못하고 동일한 숫자로 출력된다. 그렇기에
+				// 약간의 텀을 주기위해 try~catch를 이용하여 강제적으로 텀을 주어 값의 변화를 넣어줌.
 			Thread.sleep(1);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -61,4 +62,31 @@ public class newMatrixclass {
 		}
 	}
 
+	protected newMatrixclass Transpose() {
+
+		newMatrixclass result = new newMatrixclass(colCount, rowCount);
+
+		for (int r = 0; r < this.rowCount; r++) { // 행반복
+			for (int c = 0; c < this.colCount; c++) {// 열반복//Matrix의 r(행) 길이에 맞춰서 제한둠.
+				result.data[c][r] = this.data[r][c];
+			}
+		}
+		return result;
+
+	}
+
+	protected static newMatrixclass multiplyMatix(newMatrixclass m1, newMatrixclass m2) {
+		newMatrixclass result = new newMatrixclass(m1.rowCount, m2.colCount); //2*4행렬이 출력되기에 m1의 row, m2의 col이 되어야함.
+		for(int r = 0; r < result.rowCount; r++) { //count값을 생성하여 result에 넣어줬기에 result.rowcount로 작성해도 ㄱㅊ
+			for (int c = 0; c< result.colCount; c++) {
+				int value = 0; // 곱한 값을 저장할 변수 생성.
+				for(int e = 0; e < m1.colCount; e++) {
+					value += m1.data[r][e] * m2.data[e][c]; //m1, m2값이 아니라 m1의 data값, m2의 data값을 각각 곱해준다.
+				}
+				result.data[r][c] = value;
+			}
+		}
+		
+		return result;
+	}
 }
