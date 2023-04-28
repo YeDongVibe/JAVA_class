6078ae9421a348f1261f439a99e9bc713930378a
YeDongVibe <wkwhstladmss@naver.com>
2023-04-28 19:54:32 +0900

    23.04.28 study

--- edu/src/Study7_8jang3/abstractUnit.java
+++ edu/src/Study7_8jang3/abstractUnit.java
@@ -1,6 +1,6 @@
 package Study7_8jang3;
 
-public abstract class abstractUnit {
+public abstract class abstractUnit{
 	//abstract는 다중상속이 불가하다.
 	//abstract를 상속받는 class는 반드시 상위클래스의 매서드를 구현해야함.
 	abstract void run();
